02a342f09ad88921cefc6ed638944d075a2875a7
Sourav Mishra <sourav.m.bt@gmail.com>
2024-11-04 00:34:39 +0530

    Added documentation to pallet

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -1,62 +1,33 @@
-//! # DID Pallet
+//! # DID(Decentralized Identifier) Pallet
 //!
-//! A pallet with minimal functionality to help developers understand the essential components of
-//! writing a FRAME pallet. It is typically used in beginner tutorials or in Substrate template
-//! nodes as a starting point for creating a new pallet and **not meant to be used in production**.
+//! The DID Pallet provides basic functionality for managing Decentralized Identifiers (DIDs).
+//! It serves as an example to help developers understand the essential components of writing
+//! a FRAME pallet, while also introducing them to DIDs and some important functionalites that
+//! you would want to see in a DID pallet. It is intended to used for beginner tutorials as a
+//! starting point for creating a new pallet that deals with DIDs and **not meant to be used
+//! in production**.
 //!
 //! ## Overview
 //!
-//! This template pallet contains basic examples of:
-//! - declaring a storage item that stores a single `u32` value
+//! This did pallet contains examples of:
+//! - declaring a storage item that maps a `DID` to a tuple of `(DidDocument, BlockNumber)`
+//! - declaring a storage item that maps a `DID` to an `AccountId`
+//! - declaring a storage item that maps a `AccountId` to a `DID`
 //! - declaring and using events
 //! - declaring and using errors
-//! - a dispatchable function that allows a user to set a new value to storage and emits an event
-//!   upon success
-//! - another dispatchable function that causes a custom error to be thrown
-//!
-//! Each pallet section is annotated with an attribute using the `#[pallet::...]` procedural macro.
-//! This macro generates the necessary code for a pallet to be aggregated into a FRAME runtime.
-//!
-//! Learn more about FRAME macros [here](https://docs.substrate.io/reference/frame-macros/).
-//!
-//! ### Pallet Sections
-//!
-//! The pallet sections in this template are:
-//!
-//! - A **configuration trait** that defines the types and parameters which the pallet depends on
-//!   (denoted by the `#[pallet::config]` attribute). See: [`Config`].
-//! - A **means to store pallet-specific data** (denoted by the `#[pallet::storage]` attribute).
-//!   See: [`storage_types`].
-//! - A **declaration of the events** this pallet emits (denoted by the `#[pallet::event]`
-//!   attribute). See: [`Event`].
-//! - A **declaration of the errors** that this pallet can throw (denoted by the `#[pallet::error]`
-//!   attribute). See: [`Error`].
-//! - A **set of dispatchable functions** that define the pallet's functionality (denoted by the
-//!   `#[pallet::call]` attribute). See: [`dispatchables`].
-//!
-//! Run `cargo doc --package pallet-template --open` to view this pallet's documentation.
+//! - a dispatchable function that allows a user to create a DID.
+//! - a dispatchable function that allows a user to delete a DID.
+//! - a dispatchable function that allows a user to renew a DID.
 
-// We make sure this pallet uses `no_std` for compiling to Wasm.
 #![cfg_attr(not(feature = "std"), no_std)]
 
-// Re-export pallet items so that they can be accessed from the crate namespace.
 pub use pallet::*;
-
-// FRAME pallets require their own "mock runtimes" to be able to run unit tests. This module
-// contains a mock runtime specific for testing this pallet's functionality.
-// #[cfg(test)]
-// mod mock;
-
-// This module contains the unit tests for this pallet.
-// Learn about pallet unit testing here: https://docs.substrate.io/test/unit-testing/
-// #[cfg(test)]
-// mod tests;
 
 mod types;
 pub use types::*;
 
-mod traits;
-pub use traits::*;
+// mod traits;
+// pub use traits::*;
 
 // Every callable function or "dispatchable" a pallet exposes must have weight values that correctly
 // estimate a dispatchable's execution time. The benchmarking module is used to calculate weights
@@ -71,7 +42,6 @@ pub use weights::*;
 pub mod pallet {
     // Import various useful types required by all FRAME pallets.
     use super::*;
-    use crate::*;
     use frame_support::pallet_prelude::*;
     use frame_system::pallet_prelude::*;
 
@@ -111,16 +81,6 @@ pub mod pallet {
         StorageMap<Hasher = Blake2_128Concat, Key = T::AccountId, Value = DID>;
 
     /// Events that functions in this pallet can emit.
-    ///
-    ///
-    /// Events are a simple means of indicating to the outside world (such as dApps, chain explorers
-    /// or other users) that some notable update in the runtime has occurred. In a FRAME pallet, the
-    /// documentation for each event field and its parameters is added to a node's metadata so it
-    /// can be used by external interfaces or tools.
-    ///
-    ///	The `generate_deposit` macro generates a function on `Pallet` called `deposit_event` which
-    /// will convert the event type of your pallet into `RuntimeEvent` (declared in the pallet's
-    /// [`Config`] trait) and deposit it using [`frame_system::Pallet::deposit_event`].
     #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T: Config> {
@@ -150,13 +110,6 @@ pub mod pallet {
     }
 
     /// Errors that can be returned by this pallet.
-    ///
-    /// Errors tell users that something went wrong so it's important that their naming is
-    /// informative. Similar to events, error documentation is added to a node's metadata so it's
-    /// equally important that they have helpful documentation associated with them.
-    ///
-    /// This type of runtime error can be up to 4 bytes in size should you want to return additional
-    /// information.
     #[pallet::error]
     pub enum Error<T> {
         /// The DID already exists.
@@ -180,22 +133,34 @@ pub mod pallet {
     /// Dispatchable functions allows users to interact with the pallet and invoke state changes.
     /// These functions materialize as "extrinsics", which are often compared to transactions.
     /// They must always return a `DispatchResult` and be annotated with a weight and call index.
-    ///
-    /// The [`call_index`] macro is used to explicitly
-    /// define an index for calls in the [`Call`] enum. This is useful for pallets that may
-    /// introduce new dispatchables over time. If the order of a dispatchable changes, its index
-    /// will also change which will break backwards compatibility.
-    ///
-    /// The [`weight`] macro is used to assign a weight to each call.
     #[pallet::call]
     impl<T: Config> Pallet<T> {
-        /// An example dispatchable that takes a single u32 value as a parameter, writes the value
-        /// to storage and emits an event.
-        ///
-        /// It checks that the _origin_ for this call is _Signed_ and returns a dispatch
-        /// error if it isn't. Learn more about origins here: <https://docs.substrate.io/build/origins/>
         #[pallet::call_index(0)]
         #[pallet::weight(T::WeightInfo::create_did())]
+        /// Creates a new Decentralized Identifier (DID).
+        ///
+        /// This function allows a user to create a new DID with associated metadata. It performs several checks:
+        /// - Ensures the extrinsic was signed.
+        /// - Checks if the DID already exists.
+        /// - Validates the format of the DID.
+        ///
+        /// If all checks pass, the DID is added to storage and an event is emitted.
+        ///
+        /// # Arguments
+        ///
+        /// * `origin` - The origin of the extrinsic, which must be signed.
+        /// * `did` - The Decentralized Identifier to be created.
+        /// * `metadata` - Metadata associated with the DID.
+        ///
+        /// # Errors
+        ///
+        /// Returns an error if:
+        /// - The DID already exists.
+        /// - The DID format is invalid.
+        ///
+        /// # Events
+        ///
+        /// Emits a `DidCreated` event upon successful creation of the DID.
         pub fn create_did(origin: OriginFor<T>, did: DID, metadata: DidMetadata) -> DispatchResult {
             // Check that the extrinsic was signed and get the signer.
             let who = ensure_signed(origin)?;
@@ -222,18 +187,41 @@ pub mod pallet {
 
         #[pallet::call_index(1)]
         #[pallet::weight(T::WeightInfo::create_did())]
+        /// Deletes an existing Decentralized Identifier (DID).
+        ///
+        /// This function allows a user to delete an existing DID. It performs several checks:
+        /// - Ensures the extrinsic was signed.
+        /// - Checks if the DID exists.
+        /// - Ensures the DID belongs to the signer.
+        ///
+        /// If all checks pass, the DID is removed from storage and an event is emitted.
+        ///
+        /// # Arguments
+        ///
+        /// * `origin` - The origin of the extrinsic, which must be signed.
+        /// * `did` - The Decentralized Identifier to be deleted.
+        ///
+        /// # Errors
+        ///
+        /// Returns an error if:
+        /// - The DID does not exist.
+        /// - The DID does not belong to the signer.
+        ///
+        /// # Events
+        ///
+        /// Emits a `DidDeleted` event upon successful deletion of the DID.
         pub fn delete_did(origin: OriginFor<T>, did: DID) -> DispatchResult {
             // Check that the extrinsic was signed and get the signer.
             let who = ensure_signed(origin)?;
+
+            let did_from_storage = DidReverseLookup::<T>::get(&who).unwrap();
+            ensure!(did_from_storage == did, Error::<T>::NotOwnedDid); // Redundant check, but throws a better error >_<
 
             // Check if the DID exists
             ensure!(
                 Self::check_did_existence(did, who.clone()),
                 Error::<T>::DidDoesNotExist
             );
-
-            let did_from_storage = DidReverseLookup::<T>::get(&who).unwrap();
-            ensure!(did_from_storage == did, Error::<T>::NotOwnedDid); // Redundant check, but throws a better error >_<
 
             // Remove from storages.
             Self::remove_did_from_storages(did, who.clone());
@@ -247,6 +235,29 @@ pub mod pallet {
 
         #[pallet::call_index(2)]
         #[pallet::weight(T::WeightInfo::create_did())]
+        /// Renews an existing Decentralized Identifier (DID).
+        ///
+        /// This function allows a user to renew an existing DID. It performs several checks:
+        /// - Ensures the extrinsic was signed.
+        /// - Checks if the DID exists.
+        /// - Checks if the DID needs renewal based on the block number.
+        ///
+        /// If all checks pass, the block number associated with the DID is updated in storage and an event is emitted.
+        ///
+        /// # Arguments
+        ///
+        /// * `origin` - The origin of the extrinsic, which must be signed.
+        /// * `did` - The Decentralized Identifier to be renewed.
+        ///
+        /// # Errors
+        ///
+        /// Returns an error if:
+        /// - The DID does not exist.
+        /// - The DID does not need renewal.
+        ///
+        /// # Events
+        ///
+        /// Emits a `DidRenewed` event upon successful renewal of the DID.
         pub fn renew_did(origin: OriginFor<T>, did: DID) -> DispatchResult {
             // Check that the extrinsic was signed and get the signer.
             let who = ensure_signed(origin)?;
@@ -283,10 +294,24 @@ pub mod pallet {
     }
 
     impl<T: Config> Pallet<T> {
+        /// Validates the format of a Decentralized Identifier (DID).
+        ///
+        /// This function checks if the provided DID adheres to the expected format:
+        /// - The DID should be exactly 5 characters long.
+        /// - The DID should start with the prefix "did:".
+        /// - The last character of the DID should be an alphabet (uppercase or lowercase) or a digit.
+        ///
+        /// # Arguments
+        ///
+        /// * `did` - The Decentralized Identifier to be validated.
+        ///
+        /// # Returns
+        ///
+        /// Returns `true` if the DID is valid, otherwise returns `false`.
         fn is_did_valid(did: DID) -> bool {
             // Validate DID format.
-            // DID should be 5 characters long and should not have any 
-			// special characters except for alphabets and numbers and :
+            // DID should be 5 characters long and should not have any
+            // special characters except for alphabets and numbers and :
             // DID should be in the format did:x
             let did_length_check = did.len() == 5;
             let did_prefix_check = did.starts_with(b"did:");
@@ -299,12 +324,35 @@ pub mod pallet {
             did_length_check && did_prefix_check && did_suffix_check
         }
 
+        /// Checks if a Decentralized Identifier (DID) exists.
+        ///
+        /// This function checks if the provided DID exists in the storage by verifying its presence
+        /// in the `DidLookup`, `Dids`, and `DidReverseLookup` storages.
+        ///
+        /// # Arguments
+        ///
+        /// * `did` - The Decentralized Identifier to be checked.
+        /// * `who` - The account ID associated with the DID.
+        ///
+        /// # Returns
+        ///
+        /// Returns `true` if the DID exists, otherwise returns `false`.
         fn check_did_existence(did: DID, who: T::AccountId) -> bool {
             DidLookup::<T>::contains_key(did)
                 && Dids::<T>::contains_key(did)
                 && DidReverseLookup::<T>::contains_key(who)
         }
 
+        /// Adds a Decentralized Identifier (DID) to the storages.
+        ///
+        /// This function adds the provided DID and its associated metadata to the `DidLookup`, `Dids`,
+        /// and `DidReverseLookup` storages.
+        ///
+        /// # Arguments
+        ///
+        /// * `did` - The Decentralized Identifier to be added.
+        /// * `metadata` - Metadata associated with the DID.
+        /// * `who` - The account ID associated with the DID.
         fn add_did_to_storages(did: DID, metadata: DidMetadata, who: T::AccountId) {
             // Add to Lookup storage.
             DidLookup::<T>::insert(did, who.clone());
@@ -324,6 +372,15 @@ pub mod pallet {
             DidReverseLookup::<T>::insert(who.clone(), did);
         }
 
+        /// Removes a Decentralized Identifier (DID) from the storages.
+        ///
+        /// This function removes the provided DID and its associated data from the `DidLookup`, `Dids`,
+        /// and `DidReverseLookup` storages.
+        ///
+        /// # Arguments
+        ///
+        /// * `did` - The Decentralized Identifier to be removed.
+        /// * `who` - The account ID associated with the DID.
         fn remove_did_from_storages(did: DID, who: T::AccountId) {
             // Delete from Lookup storage.
             DidLookup::<T>::remove(did);
@@ -333,12 +390,6 @@ pub mod pallet {
 
             // Delete from Reverse Lookup storage.
             DidReverseLookup::<T>::remove(&who);
-        }
-
-        pub fn get_accountid_from_pubkey(pk: &PublicKey) -> Result<T::AccountId, codec::Error> {
-            // convert a publickey to an accountId
-            // TODO : Need a better way to handle the option failing?
-            T::AccountId::decode(&mut &pk[..])
         }
     }
 }
